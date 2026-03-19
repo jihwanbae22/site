dc13e667b1a58de6feddd967c6a564f814003842
jihwanbae22 <68181449+jihwanbae22@users.noreply.github.com>
2026-03-19 07:40:41 +0000

    Deploying to gh-pages from @ jihwanbae22/site@1a58381dd966ea1cd1dc77cd430fe84be929cc2f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -3,8 +3,8 @@ const ninja = document.querySelector('ninja-keys');
 
 // add the home and posts menu items
 ninja.data = [{
-    id: "nav-about",
-    title: "about",
+    id: "nav-",
+    title: "",
     section: "Navigation",
     handler: () => {
       window.location.href = "/site/";
@@ -14,7 +14,7 @@ ninja.data = [{
           description: "",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/site/";
+            window.location.href = "/site/about/";
           },
         },{id: "nav-cv",
           title: "CV",
